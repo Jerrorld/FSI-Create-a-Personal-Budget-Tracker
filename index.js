9ebcadea0813528c9ae279feddacaef1bbfd0834
Jerrorld <jerrorldk@gmail.com>
2022-05-25 20:40:01 -0700

    added script to apply budgets

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+console.log(weeklyExpenseQuestions)
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]));
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]));
+    monthlyyExpenses = monthlyExpenses + answer
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]));
+    annualExpenses = annualExpenses + answer
+}
